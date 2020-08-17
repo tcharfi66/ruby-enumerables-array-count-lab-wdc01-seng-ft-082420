cea5a618a4c06fbe858871ca76e1195471691d86
Tej Charfi <charfi@usc.edu>
2020-08-17 20:17:29 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -11,8 +11,8 @@ def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   
   array.count do 
-    |x|
-      x.to_I.empty?
+    |element|
+      element.empty?
   end
   
 end
